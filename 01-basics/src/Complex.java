7a5cb1519430a23669e93f326d6f8f981e42eed7
Adrianzatreanu <zgadi_2007@yahoo.com>
2017-03-26 01:33:50 +0200

    Add 01-basics/

---
+++ 01-basics/src/Complex.java
@@ -0,0 +1,39 @@
+public class Complex {
+    private float re;
+    private float im;
+
+    // TODO: constructor care seteaza campurile clasei
+    public Complex(float re, float im) {
+
+    }
+
+    // TODO: constructor care face campurile 0
+    public Complex() {
+
+    }
+
+    // TODO: Setter pt re
+    public void setRe(float re) {
+
+    }
+
+    // TODO: Getter pt re
+    public float getRe() {
+
+    }
+
+    // TODO: Setter pt im
+    public void setIm(float im) {
+
+    }
+
+    // TODO: Getter pt im
+    public float getIm() {
+
+    }
+
+    // TODO: afisare de genul "a + bi"
+    public String toString() {
+
+    }
+}
---
+++ 01-basics/src/ComplexOperations.java
@@ -0,0 +1,8 @@
+public class ComplexOperations {
+
+    // TODO: adds two complex numbers, return a new Complex one
+    public Complex addTwoComplexNumbers(Complex c1, Complex c2) {
+
+    }
+
+}
